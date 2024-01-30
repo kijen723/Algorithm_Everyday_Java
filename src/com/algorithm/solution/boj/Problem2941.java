0ea9d862c137676bc5b895a1c3f3869a79ecd301
kijen723 <kij2646@gmail.com>
2024-01-30 20:48:32 +0900

    create swea/Problem1220.java & swea/input1220.txt & boj/Problem2941, 15649.java && boj/Input2941, 15649.txt

---
+++ src/com/algorithm/solution/boj/Problem2941.java
@@ -0,0 +1,45 @@
+package com.algorithm.solution.boj;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+/* BOJ 2941. 크로아티아 알파벳 */
+public class Problem2941 {
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("input/boj/Input2941.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String inputStr = br.readLine();
+        int answer = 0;
+
+        for (int i = inputStr.length() - 1; i >= 0; i--) {
+            if (inputStr.charAt(i) == '=') {
+                if (inputStr.charAt(i - 1) == 'c' || inputStr.charAt(i - 1) == 's') {
+                    i--;
+                } else if (i - 2 >= 0 && inputStr.charAt(i - 2) == 'd') {
+                    i -= 2;
+                } else {
+                    i--;
+                }
+
+                answer++;
+            } else if (inputStr.charAt(i) == '-') {
+                i--;
+                answer++;
+            } else if (inputStr.charAt(i) == 'j') {
+                if (i - 1 >= 0 && (inputStr.charAt(i - 1) == 'l' || inputStr.charAt(i - 1) == 'n')) {
+                    i--;
+                }
+
+                answer++;
+            } else {
+                answer++;
+            }
+        }
+
+        System.out.println(answer);
+
+        br.close();
+    }
+}
---
+++ src/com/algorithm/solution/swea/Problem1210.java
@@ -0,0 +1,57 @@
+package com.algorithm.solution.swea;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+/* SWEA 1210. [S/W 문제해결 기본] 2일차 - Ladder1 */
+public class Problem1210 {
+    static int[][] dirs = {{0, -1}, {0, 1}};
+    static int[][] ladder = new int[100][100];
+    static boolean[][] check = new boolean[100][100];
+
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("input/swea/Input1210.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int tc = 1; tc < 11; tc++) {
+            int n = Integer.parseInt(br.readLine());
+            int start = 0;
+
+            for (int i = 0; i < 100; i++) {
+                String[] input = br.readLine().split(" ");
+
+                for (int j = 0; j < 100; j++) {
+                    ladder[i][j] = Integer.parseInt(input[j]);
+
+                    if (ladder[i][j] == 2) {
+                        start = j;
+                    }
+                }
+            }
+
+            check = new boolean[100][100];
+            int answer = go(99, start);
+
+            System.out.println("#" + tc + " " + answer);
+        }
+
+        br.close();
+    }
+
+    private static int go(int i, int j) {
+        if (i == 0) {
+            return j;
+        }
+
+        for (int[] dir : dirs) {
+            if (j + dir[1] >= 0 && j + dir[1] < 100 && !check[i][j + dir[1]] && ladder[i][j + dir[1]] == 1) {
+                check[i][j] = true;
+
+                return go(i, j + dir[1]);
+            }
+        }
+
+        return go(i - 1, j);
+    }
+}
