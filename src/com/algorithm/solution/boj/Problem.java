c3080ba75ff3d25379857928e54b29496fdab92f
kijen723 <kij2646@gmail.com>
2024-02-08 17:14:31 +0900

    create default template

---
+++ src/com/algorithm/solution/boj/Problem.java
@@ -0,0 +1,17 @@
+package com.algorithm.solution.boj;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/* BOJ */
+public class Problem {
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("input/boj/Input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        br.close();
+    }
+}
