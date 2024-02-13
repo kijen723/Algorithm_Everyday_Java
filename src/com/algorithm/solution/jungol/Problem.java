57a64230630480f269afb76060504581aaa80b3a
kijen723 <kij2646@gmail.com>
2024-02-13 15:49:46 +0900

    create JUNGOL default template

---
+++ src/com/algorithm/solution/jungol/Problem.java
@@ -0,0 +1,17 @@
+package com.algorithm.solution.jungol;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/* JUNGOL */
+public class Problem {
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("input/jungol/Input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        br.close();
+    }
+}
