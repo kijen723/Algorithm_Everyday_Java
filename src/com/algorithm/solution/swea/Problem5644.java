8f50fbe76d7dfd03cf0311840cd38c64fe848c79
kijen723 <kij2646@gmail.com>
2024-02-20 10:40:43 +0900

    create swea/Problem5644.java & swea/Input5644.txt: basic templete

---
+++ src/com/algorithm/solution/swea/Problem5644.java
@@ -0,0 +1,25 @@
+package com.algorithm.solution.swea;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/* SWEA 5644. [모의 SW 역량테스트] 무선 충전 */
+public class Problem5644 {
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("input/swea/Input5644.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int tc = 1; tc < T + 1; tc++) {
+
+
+            System.out.println("#" + tc + " ");
+        }
+
+        br.close();
+    }
+}
