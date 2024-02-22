b84633e77e6937b94d294512e3727ded57b95f5f
kijen723 <kij2646@gmail.com>
2024-02-22 17:09:32 +0900

    create boj/Problem17471.java & boj/Input17471.txt: basic templete

---
+++ src/com/algorithm/solution/boj/Problem17471.java
@@ -0,0 +1,17 @@
+package com.algorithm.solution.boj;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/* BOJ 17471. 게리맨더링 */
+public class Problem17471 {
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("input/boj/Input17471.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        br.close();
+    }
+}
