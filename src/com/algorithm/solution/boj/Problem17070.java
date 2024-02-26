101b44110f4c90ebbe162b9fb8ca1805e7460308
kijen723 <kij2646@gmail.com>
2024-02-26 22:00:59 +0900

    fix boj/Problem17070.java: Uncomplete

--- src/com/algorithm/solution/boj/Problem17070.java
+++ src/com/algorithm/solution/boj/Problem17070.java
@@ -12,6 +12,21 @@ public class Problem17070 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
+        int N = Integer.parseInt(br.readLine());
+        int[][] map = new int[N][N];
+
+        for (int r = 0; r < N; r++) {
+            st = new StringTokenizer(br.readLine());
+
+            for (int c = 0; c < N; c++) {
+                map[r][c] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+
+
+
+
         br.close();
     }
 }
