9afc52504ef2389d09aff5c4840e8da897eaa886
kijen723 <kij2646@gmail.com>
2024-02-26 20:05:05 +0900

    create boj/Problem17070.java & boj/Input17070.txt: Basic templete

---
+++ src/com/algorithm/solution/boj/Problem17070.java
@@ -0,0 +1,17 @@
+package com.algorithm.solution.boj;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/* BOJ 17070. 파이프 옮기기 1 */
+public class Problem17070 {
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("input/boj/Input17070.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        br.close();
+    }
+}
