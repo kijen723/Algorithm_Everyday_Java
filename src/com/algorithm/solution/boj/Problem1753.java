ce539dfdd4250b25bcc2ebeee27e94f034c8bb11
kijen723 <kij2646@gmail.com>
2024-02-27 14:49:57 +0900

    create boj/Problem1753.java & boj/Input1753.txt: Basic templete

---
+++ src/com/algorithm/solution/boj/Problem1753.java
@@ -0,0 +1,17 @@
+package com.algorithm.solution.boj;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/* BOJ 1753. 최단경로 */
+public class Problem1753 {
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("input/boj/Input1753.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        br.close();
+    }
+}
