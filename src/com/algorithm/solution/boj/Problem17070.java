63fea47a3152f7220ab02807adfd7e965cb15a7c
kijen723 <kij2646@gmail.com>
2024-02-27 10:02:14 +0900

    fix boj/Problem17070.java: Complete

--- src/com/algorithm/solution/boj/Problem17070.java
+++ src/com/algorithm/solution/boj/Problem17070.java
@@ -14,6 +14,7 @@ public class Problem17070 {
 
         int N = Integer.parseInt(br.readLine());
         int[][] map = new int[N][N];
+        int[][][] dp = new int[3][N][N];
 
         for (int r = 0; r < N; r++) {
             st = new StringTokenizer(br.readLine());
@@ -23,9 +24,30 @@ public class Problem17070 {
             }
         }
 
+        for (int c = 1; c < N; c++) {
+            if (map[0][c] == 0) {
+                dp[0][0][c]++;
+            } else {
+                break;
+            }
+        }
 
+        for (int r = 1; r < N; r++) {
+            for (int c = 1; c < N; c++) {
+                if (map[r][c] == 1) {
+                    continue;
+                }
 
+                dp[0][r][c] = dp[0][r][c - 1] + dp[1][r][c - 1];
+                dp[2][r][c] = dp[1][r - 1][c] + dp[2][r - 1][c];
 
+                if (map[r - 1][c] == 0 && map[r][c - 1] == 0) {
+                    dp[1][r][c] = dp[0][r - 1][c - 1] + dp[1][r - 1][c - 1] + dp[2][r - 1][c - 1];
+                }
+            }
+        }
+
+        System.out.println(dp[0][N - 1][N - 1] + dp[1][N - 1][N - 1] + dp[2][N - 1][N - 1]);
 
         br.close();
     }
