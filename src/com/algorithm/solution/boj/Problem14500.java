ed23ec6f563b6982296d9a8e78e1af284124a5dc
kijen723 <kij2646@gmail.com>
2024-02-29 08:33:42 +0900

    create boj/Problem14500.java & boj/Input14500.txt: Basic templete

---
+++ src/com/algorithm/solution/boj/Problem14500.java
@@ -0,0 +1,17 @@
+package com.algorithm.solution.boj;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/* BOJ 14500. 테트로미노 */
+public class Problem14500 {
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("input/boj/Input14500.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        br.close();
+    }
+}
