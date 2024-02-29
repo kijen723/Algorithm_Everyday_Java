84ab18f834a31a0bb3949e5ce17599f4ba1cc823
kijen723 <kij2646@gmail.com>
2024-02-29 17:08:54 +0900

    fix boj/Problem1493.java: Uncomplete

--- src/com/algorithm/solution/boj/Problem1493.java
+++ src/com/algorithm/solution/boj/Problem1493.java
@@ -12,6 +12,38 @@ public class Problem1493 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
+        st = new StringTokenizer(br.readLine());
+
+        int l = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(br.readLine());
+
+        int[] cubes = new int[20];
+
+        for (int r = 0; r < N; r++) {
+            st = new StringTokenizer(br.readLine());
+
+            int size = Integer.parseInt(st.nextToken());
+            cubes[size] = Integer.parseInt(st.nextToken());
+        }
+
+
+
         br.close();
+    }
+
+    private static void getCubes(int l, int w, int h, int cnt, int[] cubes) {
+        for (int i = cubes.length - 1; i >= 0; i++) {
+            if (cubes[i] == 0) {
+                continue;
+            }
+
+            int cubeLen = (int) Math.pow(2, i);
+
+            if (cubeLen <= l && cubeLen <= w && cubeLen <= h) {
+
+            }
+        }
     }
 }
