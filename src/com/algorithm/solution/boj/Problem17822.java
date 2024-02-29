596245789f17933507daa0bc514f617a1a736914
kijen723 <kij2646@gmail.com>
2024-02-29 10:26:33 +0900

    create boj/Problem17822.java & boj/Input17822.txt: Basic templete

---
+++ src/com/algorithm/solution/boj/Problem17822.java
@@ -0,0 +1,17 @@
+package com.algorithm.solution.boj;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/* BOJ 17822. 원판 돌리기 */
+public class Problem17822 {
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("input/boj/Input17822.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        br.close();
+    }
+}
