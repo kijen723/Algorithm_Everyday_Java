7c7aa611d1012605fa1b1d4b96d1f253af90fa46
kijen723 <kij2646@gmail.com>
2024-03-04 18:48:01 +0900

    create boj/Problem17472.java & boj/Input17472.txt: Basic templete

---
+++ src/com/algorithm/solution/boj/Problem17472.java
@@ -0,0 +1,17 @@
+package com.algorithm.solution.boj;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/* BOJ 17472. 다리 만들기 2 */
+public class Problem17472 {
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("input/boj/Input17472.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        br.close();
+    }
+}
