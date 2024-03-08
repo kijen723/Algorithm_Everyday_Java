0af106f9842356eb0c2b0f17cb1c3044c7d4c119
kijen723 <kij2646@gmail.com>
2024-03-08 16:10:11 +0900

    create boj/Problem2470.java & boj/Input2470.txt : Basic templete

---
+++ src/com/algorithm/solution/boj/Problem2470.java
@@ -0,0 +1,17 @@
+package com.algorithm.solution.boj;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/* BOJ 2470. 두 용액 */
+public class Problem2470 {
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("input/boj/Input2470.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        br.close();
+    }
+}
