b592520a61e1a84779dc863ae2757b2a1ff93377
kijen723 <kij2646@gmail.com>
2024-03-08 16:26:16 +0900

    create boj/Problem1744.java & boj/Input1744.txt : Basic templete

---
+++ src/com/algorithm/solution/boj/Problem1744.java
@@ -0,0 +1,17 @@
+package com.algorithm.solution.boj;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/* BOJ 1744. 수 묶기 */
+public class Problem1744 {
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("input/boj/Input1744.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        br.close();
+    }
+}
