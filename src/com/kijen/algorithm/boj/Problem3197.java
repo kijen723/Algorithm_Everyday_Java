12d1a7532062f10b01bb0c54060ed060cc1b002d
kijen723 <kij2646@gmail.com>
2024-03-10 13:01:34 +0900

    create boj/Problem3197.java & boj/Input3197.txt: Basic templete

---
+++ src/com/kijen/algorithm/boj/Problem3197.java
@@ -0,0 +1,17 @@
+package com.kijen.algorithm.boj;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/* BOJ 3197. 백조의 호수 */
+public class Problem3197 {
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("input/boj/Input3197.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        br.close();
+    }
+}
