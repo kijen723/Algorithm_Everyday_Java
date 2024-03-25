0cea0a57dedcfa3bfbb8850a56b81954189b8e1e
kijen723 <kij2646@gmail.com>
2024-03-25 20:03:59 +0900

    create boj/Problem4485.java & boj/Input.4485.txt

---
+++ src/com/kijen/algorithm/boj/Problem4485.java
@@ -0,0 +1,17 @@
+package com.kijen.algorithm.boj;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/* BOJ 4485. 녹색 옷 입은 애가 젤다지? */
+public class Problem4485 {
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("input/boj/Input4485.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        br.close();
+    }
+}
