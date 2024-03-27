459372ce6a21efd108186a94c42ec9754298c189
kijen723 <kij2646@gmail.com>
2024-03-27 16:46:42 +0900

    fix boj/Problem9095.java: Complete

--- src/com/kijen/algorithm/boj/Problem9095.java
+++ src/com/kijen/algorithm/boj/Problem9095.java
@@ -3,14 +3,36 @@ package com.kijen.algorithm.boj;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 /* BOJ 9095. 1, 2, 3 더하기 */
 public class Problem9095 {
     public static void main(String[] args) throws Exception {
         System.setIn(new FileInputStream("input/boj/Input9095.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int tc = 0; tc < T; tc++) {
+            int n = Integer.parseInt(br.readLine());
+
+            int[] dp;
+
+            if (n <= 3) {
+                dp = new int[4];
+            } else {
+                dp = new int[n + 1];
+            }
+
+            dp[1] = 1;
+            dp[2] = 2;
+            dp[3] = 4;
+
+            for (int i = 4; i < n + 1; i++) {
+                dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+            }
+
+            System.out.println(dp[n]);
+        }
 
         br.close();
     }
