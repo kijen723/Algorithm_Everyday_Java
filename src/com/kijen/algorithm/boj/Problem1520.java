0a3d8153f3f162c7323919a2a260c117e4a32952
kijen723 <kij2646@gmail.com>
2024-03-27 14:00:45 +0900

    fix boj/Problem1520.java: Complete

--- src/com/kijen/algorithm/boj/Problem1520.java
+++ src/com/kijen/algorithm/boj/Problem1520.java
@@ -7,10 +7,67 @@ import java.util.StringTokenizer;
 
 /* BOJ 1520. 내리막 길 */
 public class Problem1520 {
+    static int R, C;
+    static int[][] map, mem, dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+    static int dfs(int r, int c) {
+        if (r == R - 1 && c == C - 1) {
+            return 1;
+        }
+
+        if (mem[r][c] != Integer.MIN_VALUE) {
+            return mem[r][c];
+        }
+
+        int cnt = 0;
+
+        for (int[] d : dir) {
+            int nr = r + d[0];
+            int nc = c + d[1];
+
+            if (nr < 0 || nr >= R || nc < 0 || nc >= C) {
+                continue;
+            }
+
+            if (map[nr][nc] >= map[r][c]) {
+                continue;
+            }
+
+            cnt += dfs(nr, nc);
+        }
+
+        mem[r][c] = cnt;
+
+        return cnt;
+    }
+
     public static void main(String[] args) throws Exception {
         System.setIn(new FileInputStream("input/boj/Input1520.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+
+        map = new int[R][C];
+        mem = new int[R][C];
+
+        for (int r = 0; r < R; r++) {
+            st = new StringTokenizer(br.readLine());
+
+            for (int c = 0; c < C; c++) {
+                map[r][c] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int r = 0; r < R; r++) {
+            for (int c = 0; c < C; c++) {
+                mem[r][c] = Integer.MIN_VALUE;
+            }
+        }
+
+        System.out.println(dfs(0, 0));
 
         br.close();
     }
