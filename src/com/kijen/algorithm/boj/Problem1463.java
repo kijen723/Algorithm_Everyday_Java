3b64e065b58a9449a62e0275557865a5cb31f995
kijen723 <kij2646@gmail.com>
2024-03-27 16:10:10 +0900

    create boj/Problem1463.java & boj/Input1463.txt: Basic templete

---
+++ src/com/kijen/algorithm/boj/Problem1463.java
@@ -0,0 +1,17 @@
+package com.kijen.algorithm.boj;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/* BOJ 1463. 1로 만들기 */
+public class Problem1463 {
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("input/boj/Input1463.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        br.close();
+    }
+}
