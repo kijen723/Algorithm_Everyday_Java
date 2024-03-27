fca34484665d6a8799e2242d03d1dde0f2f7492e
kijen723 <kij2646@gmail.com>
2024-03-27 17:34:15 +0900

    fix boj/Problem2579.java: Complete

--- src/com/kijen/algorithm/boj/Problem2579.java
+++ src/com/kijen/algorithm/boj/Problem2579.java
@@ -3,14 +3,31 @@ package com.kijen.algorithm.boj;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 /* BOJ 2579. 계단 오르기 */
 public class Problem2579 {
     public static void main(String[] args) throws Exception {
         System.setIn(new FileInputStream("input/boj/Input2579.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] stair = new int[N + 3];
+        int[] dp = new int[N + 3];
+
+        for (int i = 1; i < N + 1; i++) {
+            stair[i] = Integer.parseInt(br.readLine());
+        }
+
+        dp[1] = stair[1];
+        dp[2] = stair[1] + stair[2];
+        dp[3] = Math.max(stair[1], stair[2]) + stair[3];
+
+        for (int i = 4; i < stair.length; i++) {
+            dp[i] = Math.max(dp[i - 3] + stair[i - 1], dp[i - 2]) + stair[i];
+        }
+
+        System.out.println(dp[N]);
 
         br.close();
     }
