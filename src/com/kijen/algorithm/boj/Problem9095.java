ff002f121e8aaed85895b59ec15ad9b87460b06e
kijen723 <kij2646@gmail.com>
2024-03-27 16:26:41 +0900

    create boj/Problem9095.java & boj/Input9095.txt: Basic templete

---
+++ src/com/kijen/algorithm/boj/Problem9095.java
@@ -0,0 +1,17 @@
+package com.kijen.algorithm.boj;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/* BOJ 9095. 1, 2, 3 더하기 */
+public class Problem9095 {
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("input/boj/Input9095.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        br.close();
+    }
+}
