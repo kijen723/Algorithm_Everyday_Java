b5754ce3401f7722f25857c53413b5da7be0ce5f
kijen723 <kij2646@gmail.com>
2024-03-27 16:48:31 +0900

    create boj/Problem2579.java & boj/Input2579.txt: Basic templete

---
+++ src/com/kijen/algorithm/boj/Problem2579.java
@@ -0,0 +1,17 @@
+package com.kijen.algorithm.boj;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/* BOJ 2579. 계단 오르기 */
+public class Problem2579 {
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("input/boj/Input2579.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        br.close();
+    }
+}
