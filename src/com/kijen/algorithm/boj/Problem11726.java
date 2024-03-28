420230ea376ad45f148f53b1e5131aae3c3c68b9
kijen723 <kij2646@gmail.com>
2024-03-29 08:56:16 +0900

    fix boj/Problem11726.java: Complete

--- src/com/kijen/algorithm/boj/Problem11726.java
+++ src/com/kijen/algorithm/boj/Problem11726.java
@@ -3,14 +3,25 @@ package com.kijen.algorithm.boj;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 /* BOJ 11726. 2×n 타일링 */
 public class Problem11726 {
     public static void main(String[] args) throws Exception {
         System.setIn(new FileInputStream("input/boj/Input11726.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[n + 2];
+
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i < n + 1; i++) {
+            dp[i] = ((dp[i - 1] % 10007) + (dp[i - 2] % 10007)) % 10007;
+        }
+
+        System.out.println(dp[n]);
 
         br.close();
     }
