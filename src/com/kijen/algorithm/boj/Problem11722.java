1ccd2a98f66fdcc77e7d38262f4e9cc152b3d2bf
kijen723 <kij2646@gmail.com>
2024-03-29 08:58:56 +0900

    create boj/Problem11722.java & boj/Input11722.txt: Basic templete

---
+++ src/com/kijen/algorithm/boj/Problem11722.java
@@ -0,0 +1,17 @@
+package com.kijen.algorithm.boj;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/* BOJ 11722. 가장 긴 감소하는 부분 수열 */
+public class Problem11722 {
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("input/boj/Input11722.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        br.close();
+    }
+}
