fed37c1ed525556fe18ac141603d169248dbbf9e
kijen723 <kij2646@gmail.com>
2024-03-28 17:28:09 +0900

    create boj/Problem11726.java & boj/Input11726.txt: Basic templete

---
+++ src/com/kijen/algorithm/boj/Problem11726.java
@@ -0,0 +1,17 @@
+package com.kijen.algorithm.boj;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/* BOJ 11726. 2×n 타일링 */
+public class Problem11726 {
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("input/boj/Input11726.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        br.close();
+    }
+}
