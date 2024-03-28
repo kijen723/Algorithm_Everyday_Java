70bd08aefb638e510c95eaf0199a53e7287ed266
kijen723 <kij2646@gmail.com>
2024-03-28 22:27:58 +0900

    fix boj/Problem14501.java: Complete

--- src/com/kijen/algorithm/boj/Problem14501.java
+++ src/com/kijen/algorithm/boj/Problem14501.java
@@ -12,6 +12,39 @@ public class Problem14501 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
+        int N = Integer.parseInt(br.readLine());
+
+        int[] T = new int[N];
+        int[] P = new int[N];
+        int[] dp = new int[31];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            T[i] = Integer.parseInt(st.nextToken());
+            P[i] = Integer.parseInt(st.nextToken());
+
+            if (i + T[i] > N) {
+                P[i] = 0;
+            }
+        }
+
+        int answer = 0;
+
+        for (int i = N - 1; i >= 0; i--) {
+            int max = Math.max(dp[i + T[i]] + P[i], dp[i + 1]);
+
+            if (dp[i] < max) {
+                dp[i] = max;
+            }
+
+            if (answer < dp[i]) {
+                answer = dp[i];
+            }
+        }
+
+        System.out.println(answer);
+
         br.close();
     }
 }
