55e1e259514fae0a13bda8405f5ffdbdc6ddc8ff
kijen723 <kij2646@gmail.com>
2024-04-06 12:52:36 +0900

    create boj/Problem1938

---
+++ src/com/kijen/algorithm/boj/Problem1938.java
@@ -0,0 +1,17 @@
+package com.kijen.algorithm.boj;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/* BOJ 1938. 통나무 옮기기 */
+public class Problem1938 {
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("input/boj/Input1938.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        br.close();
+    }
+}
