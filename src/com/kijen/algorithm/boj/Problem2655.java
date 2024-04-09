b6b2d0b28879c130d6a9f2a3ac89c76ea41d5626
kijen723 <kij2646@gmail.com>
2024-04-09 09:06:23 +0900

    create boj/Problem2655

---
+++ src/com/kijen/algorithm/boj/Problem2655.java
@@ -0,0 +1,17 @@
+package com.kijen.algorithm.boj;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/* BOJ 2655. 가장높은탑쌓기 */
+public class Problem2655 {
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("input/boj/Input2655.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        br.close();
+    }
+}
