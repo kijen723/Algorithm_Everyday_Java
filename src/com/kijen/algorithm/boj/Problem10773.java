ba759560cd741402cc97d47736e09123c6c658ab
kijen723 <kij2646@gmail.com>
2024-07-07 13:15:47 +0900

    create boj/Problem10773

---
+++ src/com/kijen/algorithm/boj/Problem10773.java
@@ -0,0 +1,17 @@
+package com.kijen.algorithm.boj;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/* BOJ 10773. 제로 */
+public class Problem10773 {
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("input/boj/Input10773.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        br.close();
+    }
+}
