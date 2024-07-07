45a40e64ef0334a3c1e0d62ea4c7cb46bea25219
kijen723 <kij2646@gmail.com>
2024-07-07 12:19:21 +0900

    fix boj/Problem11057: Complete

--- src/com/kijen/algorithm/boj/Problem11057.java
+++ src/com/kijen/algorithm/boj/Problem11057.java
@@ -7,10 +7,30 @@ import java.util.StringTokenizer;
 
 /* BOJ 11057. 오르막 수 */
 public class Problem11057 {
+    static int MODULE = 10007;
+
     public static void main(String[] args) throws Exception {
         System.setIn(new FileInputStream("input/boj/Input11057.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] dp = new int[n + 1][10];
+
+        for (int i = 0; i < n + 1; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int i = 0; i < 10; i++) {
+            dp[0][i] = 1;
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < 10; j++) {
+                dp[i][j] = ((dp[i - 1][j] % MODULE) + (dp[i][j - 1] % MODULE)) % MODULE;
+            }
+        }
+
+        System.out.println(dp[n][9]);
 
         br.close();
     }
