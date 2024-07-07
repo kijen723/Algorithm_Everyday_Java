bcc4cdc40a75a663d89b636f55a410e37d7b4f75
kijen723 <kij2646@gmail.com>
2024-07-07 12:14:43 +0900

    create boj/Problem11057

---
+++ src/com/kijen/algorithm/boj/Problem11057.java
@@ -0,0 +1,17 @@
+package com.kijen.algorithm.boj;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/* BOJ 11057. 오르막 수 */
+public class Problem11057 {
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("input/boj/Input11057.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        br.close();
+    }
+}
