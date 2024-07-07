c449c3b301b2da70fe1d4c5f352a6921a65043ef
kijen723 <kij2646@gmail.com>
2024-07-07 12:21:36 +0900

    create boj/Problem28278

---
+++ src/com/kijen/algorithm/boj/Problem28278.java
@@ -0,0 +1,17 @@
+package com.kijen.algorithm.boj;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/* BOJ 28278. 스택 2 */
+public class Problem28278 {
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("input/boj/Input28278.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        br.close();
+    }
+}
