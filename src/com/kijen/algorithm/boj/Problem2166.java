44e437666f39c67c4679a3b97732994ae6f0fc75
kijen723 <kij2646@gmail.com>
2024-07-08 14:44:11 +0900

    create boj/Problem2166

---
+++ src/com/kijen/algorithm/boj/Problem2166.java
@@ -0,0 +1,17 @@
+package com.kijen.algorithm.boj;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/* BOJ 2166. 다각형의 면적 */
+public class Problem2166 {
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("input/boj/Input2166.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        br.close();
+    }
+}
