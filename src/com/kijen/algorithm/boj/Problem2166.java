d1bf4b149d397c57d28ab6ab68c742eb83dabd23
kijen723 <kij2646@gmail.com>
2024-07-08 14:56:38 +0900

    fix boj/Problem2166: Complete

--- src/com/kijen/algorithm/boj/Problem2166.java
+++ src/com/kijen/algorithm/boj/Problem2166.java
@@ -12,6 +12,30 @@ public class Problem2166 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
+        int N = Integer.parseInt(br.readLine());
+        long[][] matrix = new long[N + 1][2];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            matrix[i][0] = Long.parseLong(st.nextToken());
+            matrix[i][1] = Long.parseLong(st.nextToken());
+        }
+
+        matrix[N][0] = matrix[0][0];
+        matrix[N][1] = matrix[0][1];
+
+        long a = 0;
+        long b = 0;
+
+        for (int i = 0; i < N; i++) {
+            a += matrix[i][0] * matrix[i + 1][1];
+            b += matrix[i][1] * matrix[i + 1][0];
+        }
+
+        double answer = (double) Math.abs(a - b) / 2;
+
+        System.out.printf("%.1f", answer);
+
         br.close();
     }
 }
