a99e5821c6610fea14af12639d7b0d10cdfb7c9c
kijen723 <kij2646@gmail.com>
2024-07-08 14:59:13 +0900

    create boj/Problem15681

---
+++ src/com/kijen/algorithm/boj/Problem15681.java
@@ -0,0 +1,17 @@
+package com.kijen.algorithm.boj;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/* BOJ 15681. 트리와 쿼리 */
+public class Problem15681 {
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("input/boj/Input15681.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        br.close();
+    }
+}
