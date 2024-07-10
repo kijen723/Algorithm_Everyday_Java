fe7a4500f616085dadfb499ca1430d406d1066a5
kijen723 <kij2646@gmail.com>
2024-07-10 11:09:08 +0900

    create boj/Problem1759

---
+++ src/com/kijen/algorithm/boj/Problem1759.java
@@ -0,0 +1,17 @@
+package com.kijen.algorithm.boj;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/* BOJ 1759. 암호 만들기 */
+public class Problem1759 {
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("input/boj/Input1759.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        br.close();
+    }
+}
